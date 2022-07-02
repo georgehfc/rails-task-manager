1ba38ece4828fb330d1d1ecddecae486624fa47f
George Chaves <george_hfc@yahoo.com.br>
2022-07-02 14:52:58 +0100

    routes, controllers and views for showing task

--- db/migrate/20220702102844_create_tasks.rb
+++ db/migrate/20220702102844_create_tasks.rb
@@ -1,10 +1,9 @@
 class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
-      t.string, :title
-      t.string, :details
-      t.boolean, :completed
-      t.false :default
+      t.string :title
+      t.string :details
+      t.boolean :completed, default: false
 
       t.timestamps
     end
